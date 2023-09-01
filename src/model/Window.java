d477ec15f90c3cb87b5f43bfe5984081de520986
Eduardo <massa-messo@hotmail.com>
2023-09-02 00:37:04 +0200

    Windowing is working

--- src/model/Window.java
+++ src/model/Window.java
@@ -4,17 +4,47 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Window {
-    private KdTree tree;
-    private float[] region;
-    private float[] range;
 
-    public Window(File file, float[] range){
-        tree = new KdTree(file);
-        region = new float[]{Float.MIN_VALUE,Float.MAX_VALUE,Float.MIN_VALUE,Float.MAX_VALUE};
-        this.range = range;
+    public static ArrayList<Point> requestTree(KdTree tree,float[] range){
+        return requestTree(tree,range,new float[]{Float.MIN_VALUE,Float.MAX_VALUE,Float.MIN_VALUE,Float.MAX_VALUE});
     }
 
-    private ArrayList<Point> reportSubtree(KdTree tree){
+    private static ArrayList<Point> requestTree(KdTree tree ,float[] range, float[] region){
+        ArrayList<Point> points = new ArrayList<>();
+        if(tree.isLeaf()){
+            Point point = tree.getPoint();
+            if(point.isInsideRange(range))
+                points.add(point);
+        }
+        else{
+            float median = tree.getMedian();
+            float[] regionLc;
+            float[] regionRc;
+            if(tree.getLine() == KdTree.Line.VERTICAL){
+                regionLc = new float[]{region[0],median,region[2],region[3]};
+                regionRc = new float[]{median,region[1],region[2],region[3]};
+            }
+            else{
+                regionLc = new float[]{region[0],region[1],region[2],median};
+                regionRc = new float[]{region[0],region[1],median,region[3]};
+            }
+            if(isFullyContained(regionLc,range)){
+                points.addAll(reportSubtree(tree.getLeft()));
+            }
+            else if(intersects(regionLc,range)){
+                points.addAll(requestTree(tree.getLeft(),range,regionLc));
+            }
+            if (isFullyContained(regionRc,range)){
+                points.addAll(reportSubtree(tree.getRight()));
+            }
+            else if (intersects(regionRc,range)){
+                points.addAll(requestTree(tree.getRight(),range,regionRc));
+            }
+        }
+        return points;
+    }
+
+    private static ArrayList<Point> reportSubtree(KdTree tree){
         ArrayList<Point> points = new ArrayList<>();
         if (tree.isLeaf()){
             points.add(tree.getPoint());
@@ -26,17 +56,24 @@ public class Window {
         return points;
     }
 
-    private boolean isFullyContained(float[] region, float[] range){
+    private static boolean isFullyContained(float[] region, float[] range){
         return region[0] >= range[0] && region[1] <= range[1] &&
                region[2] >= range[2] && region[3] <= range[3];
     }
 
-    private boolean intersects(float[] region, float[] range){
+    private static boolean intersects(float[] region, float[] range){
         if (region[1] < range[0] || region[0] > range[1])
             return false;
         else if(region[3] < range[2] || region[2] > range[3])
             return false;
         else
             return true;
+    }
+
+    public static void main(String[] args) {
+        KdTree tree = new KdTree(new File("src/data/test.txt"));
+        for(Point point : Window.requestTree(tree,new float[]{20f,30f,2000f,3000f})){
+            System.out.println("age : " + point.getX() + " revenus : " + point.getY());
+        }
     }
 }
